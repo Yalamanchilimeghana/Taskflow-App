12b603832386d939c7c6fb8315dc9d8f321c6148
Yalamanchili Meghana <ymeghana0204@gmail.com>
2026-05-12 17:45:59 +0530

    Connected frontend with deployed backend

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://taskflow-app-backend-qvdx.onrender.com"
 })
 export default instance
